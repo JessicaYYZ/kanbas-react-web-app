184cba4cfed75904d90cf8896e0b7e64e26df23d
JessicaYYZ <jessicayuyiz@gmail.com>
2024-12-12 16:09:37 -0800

    fix module

--- src/Kanbas/Courses/Modules/client.ts
+++ src/Kanbas/Courses/Modules/client.ts
@@ -15,6 +15,8 @@ export const updateModule = async (module: any) => {
 };
 
 export const deleteModule = async (moduleId: string) => {
-  const response = await axios.delete(`${MODULES_API}/${moduleId}`);
+  const response = await axiosWithCredentials.delete(
+    `${MODULES_API}/${moduleId}`
+  );
   return response.data;
 };
