dcdd29e4f10981b0b379c35263e777e425585728
JessicaYYZ <jessicayuyiz@gmail.com>
2024-12-12 16:12:28 -0800

    fix course

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -2,73 +2,40 @@ import axios from "axios";
 const axiosWithCredentials = axios.create({ withCredentials: true });
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
-
 export const createModuleForCourse = async (courseId: string, module: any) => {
-  try {
-    const response = await axiosWithCredentials.post(
-      `${COURSES_API}/${courseId}/modules`,
-      module
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Failed to create module:", error);
-    throw error;
-  }
+  const response = await axios.post(
+    `${COURSES_API}/${courseId}/modules`,
+    module
+  );
+  return response.data;
 };
 
 export const fetchAllCourses = async () => {
-  try {
-    const { data } = await axiosWithCredentials.get(COURSES_API);
-    return data;
-  } catch (error) {
-    console.error("Failed to fetch courses:", error);
-    throw error;
-  }
+  const { data } = await axiosWithCredentials.get(COURSES_API);
+  return data;
 };
 
 export const createCourse = async (course: any) => {
-  try {
-    const { data } = await axiosWithCredentials.post(COURSES_API, course);
-    return data;
-  } catch (error) {
-    console.error("Failed to create course:", error);
-    throw error;
-  }
+  const { data } = await axiosWithCredentials.post(COURSES_API, course);
+  return data;
 };
 
 export const findModulesForCourse = async (courseId: string) => {
-  try {
-    const response = await axiosWithCredentials.get(
-      `${COURSES_API}/${courseId}/modules`
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Failed to fetch modules for course:", error);
-    throw error;
-  }
+  const response = await axios.get(`${COURSES_API}/${courseId}/modules`);
+  return response.data;
 };
 
 export const deleteCourse = async (id: string) => {
-  try {
-    const { data } = await axiosWithCredentials.delete(`${COURSES_API}/${id}`);
-    return data;
-  } catch (error) {
-    console.error("Failed to delete course:", error);
-    throw error;
-  }
+  const { data } = await axiosWithCredentials.delete(`${COURSES_API}/${id}`);
+  return data;
 };
 
 export const updateCourse = async (course: any) => {
-  try {
-    const { data } = await axiosWithCredentials.put(
-      `${COURSES_API}/${course._id}`,
-      course
-    );
-    return data;
-  } catch (error) {
-    console.error("Failed to update course:", error);
-    throw error;
-  }
+  const { data } = await axiosWithCredentials.put(
+    `${COURSES_API}/${course._id}`,
+    course
+  );
+  return data;
 };
 
 export const findUsersForCourse = async (courseId: string) => {
